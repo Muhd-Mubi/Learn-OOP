104a4666347a67e678a1695606e316493c207bdf
Mubi <mubashirwasi7@gmail.com>
2023-02-02 00:26:22 +0500

    Update .cpp file
    added binary file if want to run and see
    edited .cpp file to take input from user in set functions

--- oop.cpp
+++ oop.cpp
@@ -1,30 +1,39 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Employee {
 private:
     string Name, Company;
-    int Age;
-
-public:
-    void setName(string name) {
-        Name = name;
-    }
+    int Age = 0;
+protected:
     string getName(void) {
         return Name;
-    }
-    void setCompany(string company) {
-        Company = company;
     }
     string getCompany(void) {
         return Company;
     }
-    void setAge(int age) {
-        Age = age;
-    }
     int getAge(void) {
         return Age;
+    }
+public:
+    void setName() {
+        cout << "Enter Name: ";
+        getline(cin, Name);
+        cout << endl;
+
+    }
+    void setCompany() {
+        cout << "Enter Company: ";
+        getline(cin, Company);
+
+    }
+    void setAge() {
+        cout << "Enter Age: ";
+        cin >> Age;
+        cout << endl;
     }
     void intro() {
         cout << "Name: " << getName() << endl;
@@ -35,27 +44,17 @@ public:
 };  
 
 int main(void) {
-    Employee employee1;
-    employee1.setName("Mubashir");
-    employee1.setAge(20);
-    employee1.setCompany("None");
-    employee1.intro();
-
-    Employee employee2;
-    employee2.setAge(15);
-    employee2.setName("Lassi Wala");
-    employee2.setCompany("Lassi");
-    employee2.intro();
-
-    Employee employee3;
-    employee3.setName("Talha Wasi");
-    employee3.setCompany("None");
-    employee3.setAge(16);
-    employee3.intro();
-
-    Employee employee4;
-    employee4.setName("BHAo Bhao");
-    employee4.setCompany("Quack Like A duck");
-    employee4.setAge(24);
-    employee4.intro();
+    int n;
+    cout << "Enter Number Of Employees: ";
+    cin >> n;
+    vector<Employee> employee(n);
+    for (int i = 0; i < n; i++) {
+        employee[i].setName();
+        employee[i].setCompany();
+        employee[i].setAge();
+    }
+    system("cls");
+    for (int i = 0; i < n; i++) {
+        employee[i].intro();
+    }
 }
